e0786d70e3473e73aab27deb56d90f7a156760db
Chris Earle <cmearle7@gmail.com>
2015-09-28 11:55:38 -0500

    Monster class for hack and slash

---
+++ monster.py
@@ -0,0 +1,14 @@
+class Monster:
+
+    def __init__(self, **kwargs):
+        self.hit_points = kwargs.get('hit_points', 1)
+        self.weapon = kwargs.get('weapon', 'sword')
+        self.color = kwargs.get('color', 'yellow')
+        self.sound = kwargs.get('sound', 'roar')
+
+    def battlecry(self):
+        return self.sound.upper()
+
+jubjub = Monster(color='red', sound='tweet')
+print(jubjub.sound)
+print(jubjub.weapon)
